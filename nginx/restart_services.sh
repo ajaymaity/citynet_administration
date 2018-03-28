902ce1a0ddc21ed35eee934d06614e8a3a4987d9
Corentin Cheron <chronc@tcd.ie>
2018-03-28 11:41:50 +0100

    add restart service script

---
+++ nginx/restart_services.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Base Script File (conf_rosetta.sh)
+# Created: ven. 19 janv. 2018 19:39:30 GMT
+# Version: 1.0
+#
+# This Bash script was developped by Cory.
+#
+# (c) Cory <sgryco@gmail.com>
+
+. machine_info
+
+for service in $services; do
+  echo "restarting $service";
+  ssh -p ${port} -i ${priv_key} ${user}@${IP} "sudo docker rm -f $service"
+  ssh -p ${port} -i ${priv_key} ${user}@${IP} "citynet/prod/register_${service}.sh"
+done
